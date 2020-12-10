0110cf51b86766ffa3a90d0484726e42a23606b9
nscherer30 <nscherer30@gmail.com>
2020-12-10 14:59:03 -0800

    favorites init layout

---
+++ front-end/favorites.js
@@ -0,0 +1,11 @@
+function BeerConstructor(id, name, imgUrl, category, abv,
+    type, brewer, country) {
+    this.id = id;
+    this.name = name;
+    this.imgUrl = imgUrl;
+    this.category = category;
+    this.abv = abv;
+    this.type = type;
+    this.brewer = brewer;
+    this.country = country;
+}
