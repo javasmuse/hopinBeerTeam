82e13453761e6c2e72c09115afdcd19810d27e1c
Candace Lewis <candace@Mac-mini.local>
2020-12-10 18:25:52 -0500

    beer api js and search html

--- back-end/back-end.js
+++ back-end/back-end.js
@@ -1,4 +1,3 @@
-const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const server = express();
@@ -12,17 +11,6 @@ const ONTARIO_BASE_URL = "http://ontariobeerapi.ca/beers";
 
 //route to return list of all employees
 server.get("/beers", (req, res) => {
-    // axios.get(`${ONTARIO_BASE_URL}`)
-    //     .then(function (res) {
-    //         // res.send("hi");
-    //     });
+    /
     res.send("hi");
-});
-
-//route to return employees by id
-server.get("/employees/:id", (req, res) => {
-    const eId = req.params.id;
-    const results = employees.filter((emp) => emp.eId === eId);
-
-    res.send(results);
 });
--- front-end/scripts.js
+++ front-end/scripts.js
@@ -1,7 +1,26 @@
-overAge() {
-    
-};
+document.getElementById("cellar_search").addEventListener("click", getBeers);
 
-underAge() {
-    
-};
+let bType = document.getElementById("beer_type").value;
+let bCountry = document.getElementById("country").value;
+let bName = document.getElementById("beer_name").value;
+
+function getBeers(event) {
+    event.preventDefault(event);
+
+
+    axios.get("http://ontariobeerapi.ca/beers").then((res) => {
+            let beer = res.data;
+            // console.log(beer);
+
+
+
+
+            let lagers = beer.filter(bee => bee.type === "Lager");
+            console.log(lagers);
+        }
+
+
+
+
+
+    });
