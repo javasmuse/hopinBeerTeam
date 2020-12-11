23572ee63b3677b8abe53f1175c97e4d09e3de3d
nscherer30 <nscherer30@gmail.com>
2020-12-11 10:31:02 -0800

    beer form successfully adds a new favorite beer, needs lots of CSS work

--- front-end/favorites/favorites.js
+++ front-end/favorites/favorites.js
@@ -1,5 +1,6 @@
 // DONT WORRY ABOUT CSS FOR NOW!!!
-var addFavBeerForm = document.getElementById("add-favorite-beer-form");
+const addFavBeerForm = document.getElementById("add-favorite-beer-form");
+const favoriteBeerContainer = document.getElementById("favorites-container");
 addFavBeerForm.addEventListener('submit', addFavoriteBeer);
 
 function addFavoriteBeer(event) {
@@ -7,21 +8,97 @@ function addFavoriteBeer(event) {
     console.log(event);
     console.log(event.target);
     let beerFormData = event.target;
-    let newBeer = new BeerCon
-    if (beerFormData[0].value !== undefined) {
 
-    }
+    let id = randomNumberInRange(100, 10000);
+    let name = beerFormData[0].value;
+    let imgUrl = beerFormData[1].value;
+    let category = beerFormData[2].value;
+    let abv = beerFormData[3].value;
+    let type = beerFormData[4].value;
+    let brewer = beerFormData[5].value;
+    let country = beerFormData[6].value;
+    let comments = [];
+
+    let newBeer = new Beer(id, name, imgUrl, category,
+        abv, type, brewer, country, comments);
+    console.log(newBeer);
+    // pushBeerObjToBackEnd(newBeer);
+    // add beer to view
+    prependBeerCardToContainer(favoriteBeerContainer, newBeer);
+}
+
+function createBeerCard(beerObj) {
+    // Create a container element to hold a new beer card
+    let newBeerCard = document.createElement("div");
+
+    // Create, set value and append to parent the beer name
+    let beerNameElement = document.createElement("h3");
+    beerNameElement.textContent = beerObj.name;
+    newBeerCard.appendChild(beerNameElement);
+
+    // Create, set value and append to parent the image url
+    let beerImageElement = document.createElement("img");
+    beerImageElement.src = beerObj.imgUrl;
+    newBeerCard.appendChild(beerImageElement);
+
+    // Create, set value and append to parent the beer category
+    let beerCategoryElement = document.createElement("p");
+    beerCategoryElement.innerHTML = beerObj.category;
+    newBeerCard.appendChild(beerCategoryElement);
+
+    // Create, set value and append to parent the beer abv
+    let beerAbvElement = document.createElement("p");
+    beerAbvElement.innerHTML = beerObj.abv;
+    newBeerCard.appendChild(beerAbvElement);
+
+    // Create, set value and append to parent the beer type
+    let beerTypeElement = document.createElement("p");
+    beerTypeElement.innerHTML = beerObj.type;
+    newBeerCard.appendChild(beerTypeElement);
+
+    // Create, set value and append to parent the beer brewer
+    let beerBrewerElement = document.createElement("p");
+    beerBrewerElement.innerHTML = beerObj.brewer;
+    newBeerCard.appendChild(beerBrewerElement);
+
+    // Create, set value and append to parent the beer country
+    let beerCountryElement = document.createElement("p");
+    beerCountryElement.innerHTML = beerObj.country;
+    newBeerCard.appendChild(beerCountryElement);
+
+    // Create, set value and append to parent the beer comments
+    //TODO implement view for comments
+    let beerCommentsElement = document.createElement("div");
+    beerCommentsElement.innerHTML = "TODO - IMPLEMENT COMMENTS";
+    newBeerCard.appendChild(beerCommentsElement);
+
+    // Send back the created beer card with its children
+    return newBeerCard;
+}
 
 
+function prependBeerCardToContainer(container, beerObj) {
+    let beerCard = createBeerCard(beerObj);
+    container.prepend(beerCard);
 }
 
 function pushBeerObjToBackEnd(beerObj) {
     //TODO push to heroku
 }
 
+function randomNumberInRange(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
 
 
-function BeerConstructor(id, name, imgUrl, category, abv,
+// Beer object template
+// id is beer_id from api call
+// name is name form api call
+// imgUrl is image url from api call
+// ... and so on
+// exception is comments. initialize as
+// empty array if no data is available
+function Beer(id, name, imgUrl, category, abv,
     type, brewer, country, comments) {
     this.id = id;
     this.name = name;
