ac6fe8c978ee2026dd151a058848133681a76670
nscherer30 <nscherer30@gmail.com>
2020-12-11 09:37:57 -0800

    add beer function template

--- front-end/favorites/favorites.js
+++ front-end/favorites/favorites.js
@@ -1,4 +1,25 @@
 // DONT WORRY ABOUT CSS FOR NOW!!!
+var addFavBeerForm = document.getElementById("add-favorite-beer-form");
+addFavBeerForm.addEventListener('submit', addFavoriteBeer);
+
+function addFavoriteBeer(event) {
+    event.preventDefault();
+    console.log(event);
+    console.log(event.target);
+    let beerFormData = event.target;
+    let newBeer = new BeerCon
+    if (beerFormData[0].value !== undefined) {
+
+    }
+
+
+}
+
+function pushBeerObjToBackEnd(beerObj) {
+    //TODO push to heroku
+}
+
+
 
 function BeerConstructor(id, name, imgUrl, category, abv,
     type, brewer, country, comments) {
