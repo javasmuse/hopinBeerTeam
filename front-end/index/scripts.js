8921a7d97863021c7ef07de76ee29220f1fdb798
ChrisRuaboro <52970508+ChrisRuaboro@users.noreply.github.com>
2020-12-14 08:38:47 -1000

    added favorites functionality

--- front-end/index/scripts.js
+++ front-end/index/scripts.js
@@ -50,3 +50,80 @@ function underAge() {
     // Append container to body;
     page.appendChild(container);
 };
+
+
+//Favorites stuff
+
+const HEROKU_BACK_END_BASE_URL = "https://hopin-back-end.herokuapp.com"
+const favoriteBeerContainer = document.getElementById("favorites_Container");
+// favoriteBeerContainer.addEventListener("load", loadFavoritesContainer);
+
+loadContainer(favoriteBeerContainer);
+
+function loadContainer(container) {
+    // clear beer container
+    console.log("got here")
+    container.innerHTML = "";
+    axios.get(`${HEROKU_BACK_END_BASE_URL}/user/favorites`)
+        .then(function (response) {
+            // For all favorited beer objects from heroku
+            // build beer card and
+            response.data.map(favBeer => {
+                let newBeerCard = createBeerCard(favBeer);
+                container.appendChild(newBeerCard);
+            });
+        })
+        .catch(function (error) {
+            alert(error);
+            console.log(error);
+        });
+}
+
+function createBeerCard(beerObj) {
+    // Create a container element to hold a new beer card
+    let newBeerCard = document.createElement("div");
+    newBeerCard.className = "beer-card";
+    // Create, set value and append to parent the beer name
+    let beerNameElement = document.createElement("h3");
+    beerNameElement.textContent = beerObj.name;
+    newBeerCard.appendChild(beerNameElement);
+
+    // Create, set value and append to parent the image url
+    let beerImageElement = document.createElement("img");
+    beerImageElement.src = beerObj.imgUrl;
+    newBeerCard.appendChild(beerImageElement);
+
+    // Create, set value and append to parent the beer category
+    let beerCategoryElement = document.createElement("p");
+    beerCategoryElement.innerHTML = beerObj.category;
+    newBeerCard.appendChild(beerCategoryElement);
+
+    // Create, set value and append to parent the beer abv
+    let beerAbvElement = document.createElement("p");
+    beerAbvElement.innerHTML = beerObj.abv;
+    newBeerCard.appendChild(beerAbvElement);
+
+    // Create, set value and append to parent the beer type
+    let beerTypeElement = document.createElement("p");
+    beerTypeElement.innerHTML = beerObj.type;
+    newBeerCard.appendChild(beerTypeElement);
+
+    // Create, set value and append to parent the beer brewer
+    let beerBrewerElement = document.createElement("p");
+    beerBrewerElement.innerHTML = beerObj.brewer;
+    newBeerCard.appendChild(beerBrewerElement);
+
+    // Create, set value and append to parent the beer country
+    let beerCountryElement = document.createElement("p");
+    beerCountryElement.innerHTML = beerObj.country;
+    newBeerCard.appendChild(beerCountryElement);
+
+    // Create, set value and append to parent the beer comments
+    //TODO implement view for comments
+    let beerCommentsElement = document.createElement("div");
+    beerCommentsElement.innerHTML = "TODO - IMPLEMENT COMMENTS";
+    newBeerCard.appendChild(beerCommentsElement);
+
+    // Send back the created beer card with its children
+    return newBeerCard;
+}
