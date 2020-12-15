146bbc9fe23139780b0eac62c36971e32bfe640c
ChrisRuaboro <52970508+ChrisRuaboro@users.noreply.github.com>
2020-12-14 21:55:25 -1000

    Horizontal consistency with everything

--- front-end/favorites/favorites.js
+++ front-end/favorites/favorites.js
@@ -216,6 +216,7 @@ function createBeerCard(beerObj) {
     // Create a container element to hold a new beer card
     let newBeerCard = document.createElement("div");
     newBeerCard.className = "beer-card";
+    newBeerCard.style.backgroundColor = "rgba(22,22,22,0.9)";
     // TODO Implement without showing id
     let beerIdElement = document.createElement("p");
     beerIdElement.class = "beer-id";
@@ -284,3 +285,68 @@ function createBeerCard(beerObj) {
     // Send back the created beer card with its children
     return newBeerCard;
 }
+
+
+
+
+
+
+
+
+
+//AGE VERIFICATION
+
+
+
+const ofAge = localStorage.getItem("ofAge")
+if(ofAge !== undefined && ofAge === "yes") {
+    document.getElementById("ageVerBg").style.display = "none";
+} else {
+    //flash verification
+}
+
+
+function overAge() {
+    const container = document.getElementById("ageVer");
+    container.remove();
+    const ageVerBg = document.getElementById("ageVerBg");
+    ageVerBg.remove();
+
+// store that user is above age
+// have to be global enough, check if exists
+// check if yes
+    localStorage.setItem("ofAge", "yes");
+
+
+};
+
+function underAge() {
+    const page = document.getElementsByTagName("body")[0];
+    // Create container for underage;
+    const container = document.createElement("div");
+    container.classList.add("age_Verification");
+    // Creates title and everything in it
+    const titleSpan = document.createElement("span");
+    titleSpan.classList.add("ageVerTitle");
+    let title = document.createElement("h1");
+    title.innerHTML = "Sorry!";
+    titleSpan.appendChild(title);
+    container.appendChild(titleSpan);
+    // Create sorry message
+    const bodySpan = document.createElement("span");
+    bodySpan.classList.add("ageVerBody");
+    let body = document.createElement("p");
+    body.innerHTML = "Our site is for people of age only. Don't forget us though!";
+    bodySpan.appendChild(body);
+    container.appendChild(bodySpan);
+    const exitButton = document.createElement("button");
+    exitButton.classList.add("yes");
+    exitButton.innerHTML = "Exit";
+    exitButton.addEventListener("click", function (event) {
+        window.open("https://parents.caprisun.com/");
+        window.close();
+    });
+    container.appendChild(exitButton);
+    // Append container to body;
+    page.appendChild(container);
+};
